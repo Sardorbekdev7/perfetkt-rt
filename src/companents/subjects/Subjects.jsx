cf22e9cdc9d3ac313fb067748eb8a39110317ae3
Najmiddinov Sardorbek <119738684+Sardorbekdev7@users.noreply.github.com>
2023-09-15 14:09:16 +0500

    sdcdd

--- src/companents/subjects/Subjects.jsx
+++ src/companents/subjects/Subjects.jsx
@@ -1,15 +1,17 @@
-import { Col, Row } from 'antd';
+import { Col, Collapse, Row } from 'antd';
 import './style/style.css';
 import Footer from '../footer/Footer';
 import Navbar from '../navbar/Navbar';
 import axios from 'axios';
 import { api } from '../../helps/api';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Subjects = () => {
+  const [subjects, setSubjects] = useState([]);
   const getSubjects = () => {
     axios.get(`${api}/subjects/all`).then(res => {
       console.log(res);
+      setSubjects(res.data);
     })
   }
 
@@ -24,158 +26,286 @@ const Subjects = () => {
     <div className='subjects'>
       <h1>Kafedra tomonidan o’tiladigan fanlar</h1>
       <Row>
-        <Col lg={6} md={12} sm={24} xs={24} >
-          <div className='subject-card'>
-            <h3>I - semestr</h3>
-            <table>
-              <tr>
-                <th>Fanlar</th>
-                <th>Kredit</th>
-              </tr>
-              <tr>
-                <td>Matematika</td>
-                <td>8</td>
-              </tr>
-              <tr>
-                <td>Fizika</td>
-                <td>8</td>
-              </tr>
-            </table>
-          </div>
-        </Col>
-        <Col lg={6} md={12} sm={24} xs={24} >
-          <div className='subject-card'>
-            <h3>II - semestr</h3>
-            <table>
-              <tr>
-                <th>Fanlar</th>
-                <th>Kredit</th>
-              </tr>
-              <tr>
-                <td>Matematika</td>
-                <td>8</td>
-              </tr>
-              <tr>
-                <td>Fizika</td>
-                <td>8</td>
-              </tr>
-            </table>
-          </div>
-        </Col>
-        <Col lg={6} md={12} sm={24} xs={24} >
-          <div className='subject-card'>
-            <h3>III - semestr</h3>
-            <table>
-              <tr>
-                <th>Fanlar</th>
-                <th>Kredit</th>
-              </tr>
-              <tr>
-                <td>Matematika</td>
-                <td>8</td>
-              </tr>
-              <tr>
-                <td>Fizika</td>
-                <td>8</td>
-              </tr>
-            </table>
-          </div>
-        </Col>
-        <Col lg={6} md={12} sm={24} xs={24} >
-          <div className='subject-card'>
-            <h3>IV - semestr</h3>
-            <table>
-              <tr>
-                <th>Fanlar</th>
-                <th>Kredit</th>
-              </tr>
-              <tr>
-                <td>Matematika</td>
-                <td>8</td>
-              </tr>
-              <tr>
-                <td>Fizika</td>
-                <td>8</td>
-              </tr>
-            </table>
-          </div>
-        </Col>
-        <Col lg={6} md={12} sm={24} xs={24} >
-          <div className='subject-card'>
-            <h3>V - semestr</h3>
-            <table>
-              <tr>
-                <th>Fanlar</th>
-                <th>Kredit</th>
-              </tr>
-              <tr>
-                <td>Matematika</td>
-                <td>8</td>
-              </tr>
-              <tr>
-                <td>Fizika</td>
-                <td>8</td>
-              </tr>
-            </table>
-          </div>
-        </Col>
-        <Col lg={6} md={12} sm={24} xs={24} >
-          <div className='subject-card'>
-            <h3>VI - semestr</h3>
-            <table>
-              <tr>
-                <th>Fanlar</th>
-                <th>Kredit</th>
-              </tr>
-              <tr>
-                <td>Matematika</td>
-                <td>8</td>
-              </tr>
-              <tr>
-                <td>Fizika</td>
-                <td>8</td>
-              </tr>
-            </table>
-          </div>
-        </Col>
-        <Col lg={6} md={12} sm={24} xs={24} >
-          <div className='subject-card'>
-            <h3>VII - semestr</h3>
-            <table>
-              <tr>
-                <th>Fanlar</th>
-                <th>Kredit</th>
-              </tr>
-              <tr>
-                <td>Matematika</td>
-                <td>8</td>
-              </tr>
-              <tr>
-                <td>Fizika</td>
-                <td>8</td>
-              </tr>
-            </table>
-          </div>
-        </Col>
-        <Col lg={6} md={12} sm={24} xs={24} >
-          <div className='subject-card'>
-            <h3>VIII - semestr</h3>
-            <table>
-              <tr>
-                <th>Fanlar</th>
-                <th>Kredit</th>
-              </tr>
-              <tr>
-                <td>Matematika</td>
-                <td>8</td>
-              </tr>
-              <tr>
-                <td>Fizika</td>
-                <td>8</td>
-              </tr>
-            </table>
-          </div>
-        </Col>
+        {subjects.length != 0 ? <>
+          <Col lg={6} md={12} sm={24} xs={24} >
+            <div className='subject-card'>
+              <h3>I - semestr</h3>
+              <table>
+                <tr>
+                  <th>Fanlar</th>
+                </tr>
+        {subjects.filter((e)=>e.semester==1).map((item,key)=>(
+                <tr key={key}>
+                  <td>
+                  <Collapse
+                      size="small"
+                      bordered={false}
+                      expandIconPosition={'end'}
+                      items={[
+                        {
+                          key: key + 1,
+                          label: item.subject_name,
+                          children: <p>
+                            {item.teachers.map((item1, key) => (
+                              <p>{item1.firstName} {item1.lastName}</p>
+                            ))}
+                          </p>,
+                        },
+                      ]}
+                    />
+                  </td>
+                </tr>
+                  ))}
+              </table>
+            </div>
+          </Col>
+        </>
+        :<></>}
+        {subjects.length != 0 ? <>
+          <Col lg={6} md={12} sm={24} xs={24} >
+            <div className='subject-card'>
+              <h3>II - semestr</h3>
+              <table>
+                <tr>
+                  <th>Fanlar</th>
+                </tr>
+        {subjects.filter((e)=>e.semester==2).map((item,key)=>(
+                <tr key={key}>
+                  <td>
+                  <Collapse
+                      size="small"
+                      bordered={false}
+                      expandIconPosition={'end'}
+                      items={[
+                        {
+                          key: key + 1,
+                          label: item.subject_name,
+                          children: <p>
+                            {item.teachers.map((item1, key) => (
+                              <p>{item1.firstName} {item1.lastName}</p>
+                            ))}
+                          </p>,
+                        },
+                      ]}
+                    />
+                  </td>
+                </tr>
+                  ))}
+              </table>
+            </div>
+          </Col>
+        </>
+        :<></>}
+        {subjects.length != 0 ? <>
+          <Col lg={6} md={12} sm={24} xs={24} >
+            <div className='subject-card'>
+              <h3>III - semestr</h3>
+              <table>
+                <tr>
+                  <th>Fanlar</th>
+                </tr>
+        {subjects.filter((e)=>e.semester==3).map((item,key)=>(
+                <tr key={key}>
+                  <td>
+                  <Collapse
+                      size="small"
+                      bordered={false}
+                      expandIconPosition={'end'}
+                      items={[
+                        {
+                          key: key + 1,
+                          label: item.subject_name,
+                          children: <p>
+                            {item.teachers.map((item1, key) => (
+                              <p>{item1.firstName} {item1.lastName}</p>
+                            ))}
+                          </p>,
+                        },
+                      ]}
+                    />
+                  </td>
+                </tr>
+                  ))}
+              </table>
+            </div>
+          </Col>
+        </>
+        :<></>}
+        {subjects.length != 0 ? <>
+          <Col lg={6} md={12} sm={24} xs={24} >
+            <div className='subject-card'>
+              <h3>IV - semestr</h3>
+              <table>
+                <tr>
+                  <th>Fanlar</th>
+                </tr>
+        {subjects.filter((e)=>e.semester==4).map((item,key)=>(
+                <tr key={key}>
+                  <td>
+                  <Collapse
+                      size="small"
+                      bordered={false}
+                      expandIconPosition={'end'}
+                      items={[
+                        {
+                          key: key + 1,
+                          label: item.subject_name,
+                          children: <p>
+                            {item.teachers.map((item1, key) => (
+                              <p>{item1.firstName} {item1.lastName}</p>
+                            ))}
+                          </p>,
+                        },
+                      ]}
+                    />
+                  </td>
+                </tr>
+                  ))}
+              </table>
+            </div>
+          </Col>
+        </>
+        :<></>}
+        {subjects.length != 0 ? <>
+          <Col lg={6} md={12} sm={24} xs={24} >
+            <div className='subject-card'>
+              <h3>V - semestr</h3>
+              <table>
+                <tr>
+                  <th>Fanlar</th>
+                </tr>
+        {subjects.filter((e)=>e.semester==5).map((item,key)=>(
+                <tr key={key}>
+                  <td>
+                  <Collapse
+                      size="small"
+                      bordered={false}
+                      expandIconPosition={'end'}
+                      items={[
+                        {
+                          key: key + 1,
+                          label: item.subject_name,
+                          children: <p>
+                            {item.teachers.map((item1, key) => (
+                              <p>{item1.firstName} {item1.lastName}</p>
+                            ))}
+                          </p>,
+                        },
+                      ]}
+                    />
+                  </td>
+                </tr>
+                  ))}
+              </table>
+            </div>
+          </Col>
+        </>
+        :<></>}
+        {subjects.length != 0 ? <>
+          <Col lg={6} md={12} sm={24} xs={24} >
+            <div className='subject-card'>
+              <h3>VI - semestr</h3>
+              <table>
+                <tr>
+                  <th>Fanlar</th>
+                </tr>
+        {subjects.filter((e)=>e.semester==6).map((item,key)=>(
+                <tr key={key}>
+                  <td>
+                  <Collapse
+                      size="small"
+                      bordered={false}
+                      expandIconPosition={'end'}
+                      items={[
+                        {
+                          key: key + 1,
+                          label: item.subject_name,
+                          children: <p>
+                            {item.teachers.map((item1, key) => (
+                              <p>{item1.firstName} {item1.lastName}</p>
+                            ))}
+                          </p>,
+                        },
+                      ]}
+                    />
+                  </td>
+                </tr>
+                  ))}
+              </table>
+            </div>
+          </Col>
+        </>
+        :<></>}
+        {subjects.length != 0 ? <>
+          <Col lg={6} md={12} sm={24} xs={24} >
+            <div className='subject-card'>
+              <h3>VII - semestr</h3>
+              <table>
+                <tr>
+                  <th>Fanlar</th>
+                </tr>
+        {subjects.filter((e)=>e.semester==7).map((item,key)=>(
+                <tr key={key}>
+                  <td>
+                  <Collapse
+                      size="small"
+                      bordered={false}
+                      expandIconPosition={'end'}
+                      items={[
+                        {
+                          key: key + 1,
+                          label: item.subject_name,
+                          children: <p>
+                            {item.teachers.map((item1, key) => (
+                              <p>{item1.firstName} {item1.lastName}</p>
+                            ))}
+                          </p>,
+                        },
+                      ]}
+                    />
+                  </td>
+                </tr>
+                  ))}
+              </table>
+            </div>
+          </Col>
+        </>
+        :<></>}
+        {subjects.length != 0 ? <>
+          <Col lg={6} md={12} sm={24} xs={24} >
+            <div className='subject-card'>
+              <h3>VIII - semestr</h3>
+              <table>
+                <tr>
+                  <th>Fanlar</th>
+                </tr>
+        {subjects.filter((e)=>e.semester==8).map((item,key)=>(
+                <tr key={key}>
+                  <td>
+                  <Collapse
+                      size="small"
+                      bordered={false}
+                      expandIconPosition={'end'}
+                      items={[
+                        {
+                          key: key + 1,
+                          label: item.subject_name,
+                          children: <p>
+                            {item.teachers.map((item1, key) => (
+                              <p>{item1.firstName} {item1.lastName}</p>
+                            ))}
+                          </p>,
+                        },
+                      ]}
+                    />
+                  </td>
+                </tr>
+                  ))}
+              </table>
+            </div>
+          </Col>
+        </>
+        :<></>}
       </Row>
     </div>
     <Footer />
